4d440aabe4f1f8aa4818ef26f9c63aab75699d16
sonusah395 <107795227+sonusah395@users.noreply.github.com>
2022-09-22 00:14:49 +0530

    #include<stdio.h> int main() {     int i,j,r,c;     //scanf("%d",&n);     scanf("%d%d",&r,&c);     int a[r][c];
    for(i=0;i<r;i++)
    {
     for(j=0;j<c;j++)
     {
         scanf("%d",&a[i][j]);
     }
    }
    int sum=0;
    for(i=0;i<r;i++)
    {
        for(j=0;j<c;j++)
        {
                sum= sum+a[i][j];
        }
    }
    printf("%d",sum);
    return 0;
    }

---
+++ Sum_of_matrix_elements.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+    int i,j,r,c;
+    //scanf("%d",&n);
+    scanf("%d%d",&r,&c);
+    int a[r][c];
+    
+for(i=0;i<r;i++)
+{
+ for(j=0;j<c;j++)
+ {
+     scanf("%d",&a[i][j]);
+ }
+}
+int sum=0;
+for(i=0;i<r;i++)
+{
+    for(j=0;j<c;j++)
+    {
+            sum= sum+a[i][j];
+    }
+}
+printf("%d",sum);
+return 0;
+}
